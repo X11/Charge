1e61f69e173a64924e8bf47f70c3a87547a63574
Ikzelf01 <ikzelf01@live.nl>
2015-01-09 15:13:20 +0100

    God

--- server/app.js
+++ server/app.js
@@ -4,6 +4,32 @@ var container = require('./container.js');
 var game = new container.make(40, 40);
 
 io.sockets.on('connection', function(socket){
-    var l = game.grid.length;
-    console.log(l, game.grid[0].length);
+    
+    console.log('Socket connected');
+
+    socket.on('request_grid', function(){
+        console.log(this.id + ' requesting grid layout');
+        this.emit('receive_grid', {
+            rows: game.grid.length,
+            cols: game.grid[0].length
+        });
+    });
+
+    socket.on('request_playing', function(){
+        if (game.players.length < 3) {
+            console.log(this.id + ' inserted into game queue');
+            game.players.push(this);
+            this.emit('receive_status', {status: 'waiting'});
+        } else {
+            console.log(this.id + ' denied from game queue');
+            this.emit('notice', {message: 'To many players'});
+        }
+    });
+    
+    socket.on('disconnect', function(){
+        if (game.players.indexOf(this) != -1){
+            console.log(this.id + ' disconnected, Removed from waiting queue');
+        }
+    });
+
 });
