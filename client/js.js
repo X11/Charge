64edc3531a75ed93515147a4b420b06540fd9abf
Imakethings <Mirkovdw@outlook.com>
2015-01-09 14:53:38 +0100

    Layout

--- client/js.js
+++ client/js.js
@@ -1,14 +1,31 @@
+var vals = {'empty': 0, 'wall': 1, 'player': 2, 'trail': 3}
+
 function Container() 
 {
     this.tiles = [[], []]
 
+    this.container = document.querySelector('.container');
 
+    for (var r = 0; r < 40; r++) 
+    {
+        this.tiles[r] = []
+        for (var c = 0; c < 40; c++) 
+        {
+            this.tiles[r][c] = new Tile(r, c)
+            this.container.appendChild(this.tiles[r][c].div);
+        }
+    }
 }
 
-function Tile(){
-    this.value 0;
-    this
+function Tile(x, y){
+    this.div = document.createElement('div');
+    this.div.className = 'container-child';
 
+    this.x = x;
+    this.y = y;
+    this.val = 0;
+    this.setValue = function() {}
+    this.checkUpdate = function() {}
 }
 
 var Screen = new Container()
