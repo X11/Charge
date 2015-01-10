acca90f3b9c50926fa00b61ecd41810532175233
Ikzelf01 <ikzelf01@live.nl>
2015-01-10 21:12:43 +0100

    Great improvement

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 
 var io = require('socket.io').listen(2424);
-var container = require('./container.js');
-var game = new container.make(40, 40);
+var Container = require('./container.js');
+var game = new Container.Make(40, 40);
 
 io.sockets.on('connection', function(socket){
     
@@ -18,7 +18,7 @@ io.sockets.on('connection', function(socket){
     socket.on('request_playing', function(){
         if (game.players.length < 3) {
             console.log(this.id + ' inserted into game queue');
-            game.players.push(this);
+            game.players.push(new Container.Player({socket:this}));
             this.emit('receive_status', {status: 'waiting'});
         } else {
             console.log(this.id + ' denied from game queue');
@@ -27,9 +27,22 @@ io.sockets.on('connection', function(socket){
     });
     
     socket.on('disconnect', function(){
-        if (game.players.indexOf(this) != -1){
+        if (game.isPlayer(this.id)){    
             console.log(this.id + ' disconnected, Removed from waiting queue');
         }
     });
 
 });
+
+var refresh_rate = 60;
+var refresh_timer = null;
+var checkForStart = setInterval(function(){
+    if (game.players.length > 1){
+        clearInterval(checkForStart);
+        game.start();
+        refresh_timer = setInterval(function(){
+            game.move();
+            game.print();
+        }, refresh_rate);
+    }
+}, 3000);
--- server/container.js
+++ server/container.js
@@ -3,24 +3,114 @@ var Tile = function(value){
     this.value = value;
 }
 
-var Container = function (rows, cols){
+var Game = function (rows, cols){
     
+    this.rows = rows;
+    this.cols = cols;
     this.players = [];
     this.grid = [[],[]];
     
     for (r=0;r<rows;r++){
         this.grid[r] = [];
         for (c=0;c<cols;c++){
-            this.grid[r][c] = new Tile('empty');
+            if (r == 0 || r == rows-1 || c == 0 || c == cols-1)
+                this.grid[r][c] = new Tile('wall');
+            else
+                this.grid[r][c] = new Tile('empty');
         }
     }
     
-    this.isPlayer = function(socket){
-        if (this.players[socket.id])
-            return true;
+    this.isPlayer = function(id){
+        for (i in this.players)
+            if (this.players[i].socket.id == id)
+                return true;
         return false;
+    }
+
+    this.start = function(){
+        var i;
+        var l = this.players.length;
+        for (i=0;i<l;i++){
+            var cur = this.players[i];
+            if (l == 3 && i == 0){
+                cur.x = Math.floor(this.rows/2);
+                cur.y = Math.floor(this.rows/3);
+                this.dir = 'S';
+            } else {
+                var add = 1;
+                if (l==3)
+                    add = 0;
+                cur.x = Math.floor(this.rows/3*2);
+                cur.y = Math.floor(this.rows/3*(i+add));
+            }
+            this.grid[cur.x][cur.y].value = cur.socket.id;
+        }
+    }
+
+    this.move = function(){
+        for (i in this.players){
+            var cur = this.players[i];
+            var newPos = [cur.x, cur.y];
+            var currentTile = this.grid[cur.x][cur.y];
+            switch(cur.dir){
+                case 'N':
+                    newPos[0]-=1;
+                    break;
+                case 'E':
+                    newPos[1]+=1;
+                    break;
+                case 'S':
+                    newPos[0]+=1;
+                    break;
+                case 'W':
+                    newPos[1]-=1;
+                    break;
+            }
+            if (this.checkCollision(newPos)){
+                // Player will die
+                continue;
+            }
+            //console.log(cur.socket.id, ' moving ', newPos);
+            currentTile.value = 'wall';
+            cur.x = newPos[0];
+            cur.y = newPos[1];
+            this.grid[cur.x][cur.y].value = cur.socket.id;
+        }
+    }
+
+    this.checkCollision = function(pos){
+        if(this.grid[pos[0]][pos[1]].value == 'empty')
+            return false;
+        return true;
+    }
+
+    this.print = function(){
+        for (var r = 0; r < this.grid.length; r++){
+            var p = '';
+            for (var c = 0; c < this.grid[r].length; c++){
+                switch(this.grid[r][c].value){
+                    case 'empty':
+                        p += ' ';
+                        break;
+                    case 'wall':
+                        p += 'X';
+                        break;
+                    default:
+                        p+= '0';
+                }
+            }
+            console.log(p);
+        }
     }
 
 }
 
-module.exports.make = Container;
+function Player(options){
+    this.x = 0;
+    this.y = 0;
+    this.dir = 'N';
+    this.socket = options.socket;
+}
+
+module.exports.Make = Game;
+module.exports.Player = Player;
