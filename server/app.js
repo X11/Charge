f893b37d26e188f497f9ffd77d14d43f54058904
Ikzelf01 <ikzelf01@live.nl>
2015-01-16 12:58:16 +0100

    Tweaks

--- server/app.js
+++ server/app.js
@@ -12,6 +12,41 @@ game.spawnpoints = [
 ];
 var players = [];
 
+
+function convertHtmlToText(returnText) {
+    //-- remove BR tags and replace them with line break
+    returnText=returnText.replace(/<br>/gi, "\n");
+    returnText=returnText.replace(/<br\s\/>/gi, "\n");
+    returnText=returnText.replace(/<br\/>/gi, "\n");
+
+    //-- remove P and A tags but preserve what's inside of them
+    returnText=returnText.replace(/<p.*>/gi, "\n");
+    returnText=returnText.replace(/<a.*href="(.*?)".*>(.*?)<\/a>/gi, " $2 ($1)");
+    returnText=returnText.replace(/<h.*>(.*?)<\/a>/gi, " $2 ($1)");
+
+    //-- remove all inside SCRIPT and STYLE tags
+    returnText=returnText.replace(/<script.*>[\w\W]{1,}(.*?)[\w\W]{1,}<\/script>/gi, "");
+    returnText=returnText.replace(/<style.*>[\w\W]{1,}(.*?)[\w\W]{1,}<\/style>/gi, "");
+    //-- remove all else
+    returnText=returnText.replace(/<(?:.|\s)*?>/g, "");
+
+    //-- get rid of more than 2 multiple line breaks:
+    returnText=returnText.replace(/(?:(?:\r\n|\r|\n)\s*){2,}/gim, "\n\n");
+
+    //-- get rid of more than 2 spaces:
+    returnText = returnText.replace(/ +(?= )/g,'');
+
+    //-- get rid of html-encoded characters:
+    returnText=returnText.replace(/&nbsp;/gi," ");
+    returnText=returnText.replace(/&amp;/gi,"&");
+    returnText=returnText.replace(/&quot;/gi,'"');
+    returnText=returnText.replace(/&lt;/gi,'<');
+    returnText=returnText.replace(/&gt;/gi,'>');
+
+    //-- return
+    return returnText;
+}
+
 io.sockets.on('connection', function(socket){
     
     console.log(socket.id);   
@@ -57,6 +92,7 @@ io.sockets.on('connection', function(socket){
     
     socket.on('send_message', function (data) {
         var user = players[this.id].color;
+        data,msg = convertHtmlToText(data.msg);
         for (i in players)
             players[i].socket.emit('on_message', {user: user, msg: data.msg});
         console.log(user, data.msg);
